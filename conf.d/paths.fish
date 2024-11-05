fad42d1047f80ba2d82555262797e0b329a682b9
jgusta <jgustafx@gmail.com>
2024-11-05 05:55:25 -0800

    refactoring

--- conf.d/paths.fish
+++ conf.d/paths.fish
@@ -1,13 +1,16 @@
 function ___paths_plugin_set_colors
-    set -q ___paths_plugin_colors or
-    set -U ___paths_plugin_colors 7343A9 9B0F8C B8007F CE2029 D84528 E26928 EB8E27 F5B227 FFD726 FFE675 FFF6CC FFEEE2 FFF5FF 10EDF5 28BCE0 556CBC 7343A9 9B0F8C B8007F CE2029 D84528 E26928 EB8E27 F5B227 FFD726 FFE675 FFF6CC FFEEE2 FFF5FF 10EDF5 28BCE0 556CBC
+    if not set -q ___paths_plugin_colors
+        set -Ux ___paths_plugin_colors 27e6ff 29e0ff 5cd8ff 77d0ff 8ac8ff 9cbfff afb5ff c5a7ff d99bfe ea8feb f684d5 fe7abd ff73a3 ff708a fa7070 ff708a ff73a3 fe7abd f684d5 ea8feb d99bfe c5a7ff afb5ff 9cbfff 8ac8ff 77d0ff 5cd8ff 29e0ff
+    end
+    return 0
 end
 
 function _paths_uninstall --on-event paths_uninstall
-    for i in ___paths_plugin_wrap_color ___paths_plugin_output ___paths_plugin_handle_found_item ___paths_plugin_handle_source
+    for i in ___paths_plugin_wrap_color ___paths_plugin_output ___paths_plugin_handle_found_item ___paths_plugin_handle_source ___paths_plugin_cycle_color
         functions -e $i
     end
     set -e ___paths_plugin_colors
+    set -e ___paths_plugin_current_color
 end
 
 function _paths_install --on-event _paths_install
--- functions/paths.fish
+++ functions/paths.fish
@@ -1,174 +1,141 @@
 function ___paths_plugin_wrap_color
     set_color normal
     set_color "$argv[1]"
-    echo -n "$argv[2..]"
+    echo -n (set_color "$argv[1]")"$argv[2..]"
     set_color normal
 end
 
-function ___paths_plugin_output
-    set -l options (fish_opt -s c -l color --required-val)
-    set -a options (fish_opt -s p -l no-prefix) # no prefix
-    set -a options (fish_opt -s k -l clean) # no ___paths_plugin_colors
-    set -a options (fish_opt -s n -l no-endline) # echos without newline
-
-    argparse $options -- $argv
-
-    set -l acc ''
-    set -l tick -
-    set -l listy ""
-
-    # -c/--color <color> means to color this line with <color>
-    if set -q _flag_c
-        # include tick?
-        if not set -q _flag_p
-            # color tick?
-            if not set -q _flag_k
-                set tick (___paths_plugin_wrap_color "yellow" "$tick")
-            end
-        end
-        # wrap line in color or not
-        if set -q _flag_k
-            set listy $argv
-        else
-            set listy (___paths_plugin_wrap_color $_flag_c "$argv")
-        end
-    else
-        # include tick?
-        if not set -q _flag_p
-            # we still color the tick unless -k is set
-            if not set -q _flag_k
-                set tick (___paths_plugin_wrap_color "yellow" "$tick")
-            end
-        end
-        # do not wrap line in color
-        set listy $argv
+function ___paths_plugin_cycle_color
+    if not set -q ___paths_plugin_current_color
+        set -Ux ___paths_plugin_current_color 1
+    else if test $___paths_plugin_current_color -gt (count $___paths_plugin_colors)
+        set -Ux ___paths_plugin_current_color 1
     end
-
-    # Bring line together with or without tick
-    if set -q _flag_p
-        set acc "$listy"
-    else
-        set acc "$tick $listy"
-    end
-
-    # echo line with or without new line
-    if set -q _flag_n
-        echo -n "$acc"
-    else
-        echo -n -e "$acc\n"
-    end
+    echo $___paths_plugin_colors[$___paths_plugin_current_color]
+    set -Ux ___paths_plugin_current_color (math $___paths_plugin_current_color + 1)
 end
 
-function ___paths_plugin_handle_found_item -S --argument-names testName outFlags
-    # re-split flags into list
-    set outFlags (string split " " $outFlags)
+function ___paths_plugin_handle_found_item -a testName outFlags
+    set -f flags (string split -n ' ' -- "$outFlags")
+    set -f options (fish_opt -s c -l clean)
+    set -a options (fish_opt -s s -l single)
+    set -a options (fish_opt -s k -l no-color)
+    set -a options (fish_opt -s n -l inline)
+    argparse $options -- $flags
+
+    set -f arrow "=>"
     # check if file exists
     if test -e "$testName"
-        set -f nameOut "$testName"
-        # check if file is a symlink
-        if test -L "$testName"
-            if set -q _flag_q
-                or set -q _flag_s
-                :
-            else
+        set -f nameOut (string trim -- "$testName")
+        if not set -q _flag_c # is not clean
+            if test -L "$testName" # is symlink
                 set -f __linkname (readlink -f "$testName")
-                set -f arrow "=>"
-                if not set -q NO_COLOR
-                    set __linkName (___paths_plugin_wrap_color "$___paths_plugin_colors[$i]" $__linkname)
-                    set arrow (___paths_plugin_wrap_color "yellow" "$arrow")
+                set __linkname (string trim -- "$__linkname")
+                set testName (string trim -- "$testName")
+                if not set -q _flag_k # is color
+                    set nameOut (___paths_plugin_wrap_color (___paths_plugin_cycle_color) $testName) (___paths_plugin_wrap_color "yellow" "$arrow") (___paths_plugin_wrap_color (___paths_plugin_cycle_color) $__linkname)
+                else # is color
+                    set nameOut (echo -n "$testName" "$arrow" "$__linkname")
                 end
-                set testName (___paths_plugin_output --color="$___paths_plugin_colors[$i]" $outFlags -n "$testName")
-                set nameOut "$testName $arrow $__linkname"
+            else # is not symlink
+                if not set -q _flag_k # is color
+                    set testName (string trim -- "$testName")
+                    set nameOut (___paths_plugin_wrap_color (___paths_plugin_cycle_color) "$testName")
+                else
+                    set testName (string trim -- "$testName")
+                    set nameOut "$testName"
+                end
             end
-        else
-            set nameOut (___paths_plugin_output --color="$___paths_plugin_colors[$i]" $outFlags -n "$testName")
-        end
-        echo "$nameOut"
-    end # [end check file name]
-end
 
-function ___paths_plugin_handle_source --argument-names source acc
-    #echo "i am run" "$source : $acc"
-    # only print anything if there is anything.
-    if set -q acc
-        # determine if we print the source or not
-        if not set -q _flag_q
-            and not set -q _flag_s
-            # prepend source
-            set -p acc "$source\n"
-            set -a acc "\n"
+            set nameOut (string trim -- "$nameOut")
+            # do the tick 
+            if set -q _flag_k # is not color
+                set nameOut "- $nameOut"
+            else # is color
+                set nameOut (___paths_plugin_wrap_color "yellow" "- ") "$nameOut"
+            end
         end
-
-        # echo the whole section
-        echo -e "$acc"
+        set nameOut (string trim -- "$nameOut")
+        echo -n $nameOut
     end
 end
 
 function paths --description "Reveal the executable matches in shell paths or fish autoload."
-    set options (fish_opt -s q -l clean)
+    set -f options (fish_opt -s c -l clean)
     set -a options (fish_opt -s s -l single)
-    set -a options (fish_opt -s n -l no-color)
+    set -a options (fish_opt -s k -l no-color)
+    set -a options (fish_opt -s n -l inline)
     argparse $options -- $argv
-    
+
     if test (count $argv) -lt 1
         echo "paths - executable matches in shell paths or fish autoload."
-        and echo "usage: paths [-q|-s|-n] <name>"
-        and echo -e "\t-q or --clean: output without color or headers"
+        and echo "usage: paths [-c|-s|-k] <name>"
+        and echo -e "\t-c or --no-color: output without color"
         and echo -e "\t-s or --single: output without color or headers, the first result"
-        and echo -e "\t-n or --no-color: output without color"
+        and echo -e "\t-k or --clean: output without tick marks or headers"
+        # and echo -e "\t-n or --inline: output without endline"
         and return 1
     end
 
     set -f foundStatus 1
     set -f input $argv
+    if set -q _flag_s
+        set _flag_k True
+        set _flag_c True
+    end
     set -f outFlags ''
-
-    # determine no color flag
-    if set -q NO_COLOR
-        or set -q _flag_n
-        or set -q _flag_q
-        or set -q _flag_s
-        set -f NO_COLOR
-        set -p outFlags -k
-    end
-
-    # determine no prefix flag
-    if set -q _flag_q
-        or set -q _flag_s
-        set -p outFlags -p
-    end
-
+    set -q _flag_n; and set -a outFlags -n
+    set -q _flag_c; and set -a outFlags -c
+    set -q _flag_k; and set -a outFlags -k
+    set -q _flag_s; and set -a outFlags -s
+    set outFlags (string split -n " " -- "$outFlags")
     ___paths_plugin_set_colors
-
-    set -f i (count $___paths_plugin_colors)
-
     # loop over list of path lists
     for pVar in VIRTUAL_ENV fisher_path fish_function_path fish_user_paths PATH
         set -e acc
-        set -f acc
-
+        set -f acc ''
+        set -e hit
         # see if variable is empty
         if test -z "$pVar"
             continue
         end
+        set -f acc (begin
+            for t in $$pVar
+                for snit in "$t/$input.fish" "$t/$input"
+                    set -f found (___paths_plugin_handle_found_item "$snit" "$outFlags")
+                    set found (string trim -- "$found")
+                    if test -n "$found"
+                        set -f hit True
+                        echo "$found"
+                        if set -q _flag_s
+                            break
+                        end
+                    end
+                end
+                if set -q _flag_s
+                    if set -q hit
+                        break
+                    end
+                end
+            end
+        end)
 
-        # loop over path lists
-        for t in $$pVar
-            # loop over files with and without fish 
-            for name in "$t/$input.fish" "$t/bin/$input" "$t/$input"
-                set -a acc (___paths_plugin_handle_found_item "$name" "$outFlags")
+        # prepend source
+        if not set -q _flag_c
+            if set -q hit
+                set pVar (string trim -- "$pVar")
+                echo -e -n "$pVar\n"
             end
         end
 
-        set -p acc (___paths_plugin_handle_source "$pVar" "$acc")
-
-        # next color
         if test -n "$acc"
             set foundStatus 0
-            echo -e "$acc"
-            set i (math $i - 1)
-            if set -q _flag_s
-                return $foundStatus
+            for fk in $acc
+                echo $fk
+                if set -q _flag_s
+                    # stop after one
+                    return $foundStatus
+                end
             end
         end
     end
