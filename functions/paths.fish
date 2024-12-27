69f91a608a0e2df5122e5e96ec67ac9b46566433
jgusta <jgustafx@gmail.com>
2024-12-26 20:47:06 -0800

    fix function not found issue

--- functions/paths.fish
+++ functions/paths.fish
@@ -5,6 +5,14 @@ function ___paths_plugin_wrap_color
     set_color normal
 end
 
+# duplicated in conf.d
+function ___paths_plugin_set_colors
+    if not set -q ___paths_plugin_colors
+        set -Ux ___paths_plugin_colors 27e6ff 29e0ff 5cd8ff 77d0ff 8ac8ff 9cbfff afb5ff c5a7ff d99bfe ea8feb f684d5 fe7abd ff73a3 ff708a fa7070 ff708a ff73a3 fe7abd f684d5 ea8feb d99bfe c5a7ff afb5ff 9cbfff 8ac8ff 77d0ff 5cd8ff 29e0ff
+    end
+    return 0
+end
+
 function ___paths_plugin_cycle_color
     if not set -q ___paths_plugin_current_color
         set -Ux ___paths_plugin_current_color 1
