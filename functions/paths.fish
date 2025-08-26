198c5c14e94c03ff910810a31988fd2fb8b67045
jgusta <jgustafx@gmail.com>
2025-08-25 19:23:27 -0700

    version 1.2.0

--- functions/paths.fish
+++ functions/paths.fish
@@ -3,59 +3,105 @@
 # by jgusta (https://github.com/jgusta)
 set -gx VERSION 1.2.0
 
-# function ___paths_plugin_help
-#     echo "paths - executable matches in shell paths or fish autoload."
-#     echo "Usage: paths [-c|-s|-k] <name>"
-#     echo "Arguments:"
-#     echo "    <name> - name of a fish autoload function, function, shell script, executable or builtin"
-#     echo ""
-#     echo "Options:"
-#     echo ""
-#     echo "-k, --no-color"
-#     echo "    Output without color"
-#     echo ""
-#     echo "-c, --clean"
-#     echo "    Output without tick marks, headers or saymlink destination."
-#     echo "    Implies -k."
-#     echo ""
-#     echo "-s, --single:"
-#     echo "    Output the first result. Implies -k -c"
-#     echo ""
-#     echo "-f, --fail-if-not-path"
-#     echo "    If the first result that would be output is not an executable file"
-#     echo "    then return nothing and exit with status 1. Use if you want to make sure"
-#     echo "    that the output is executable before you run it."
-#     echo ""
-#     echo "Explaination:"
-#     echo ""
-#     echo "    paths is a fish function (similar to ```type -a```) that takes a command name and walks"
-#     echo "    through each of the executable locations to see where the command will"
-#     echo "    execute from. Once found, it continues to find each subsequent location"
-#     echo "    that are next in line were the first command be removed (with exceptions*)."
-#     echo "    Commands are listed in priority order with a heading for each group of"
-#     echo "    executable listing."
-#     echo ""
-#     echo "Limitations:"
-#     echo ""
-#     echo "  * For declared functions such as those loaded by config.fish only the"
-#     echo "    latest sourced function will be listed. i.e. if the function is overwritten,"
-#     echo "    paths will not be able to tell what it was before it was overwritten. "
-#     echo "    In the event of a function defined via stdin into the source command,"
-#     echo "    the output will path will be '-' unless the `--fail-if-not-path` option"
-#     echo "    is set."
-#     echo ""
-#     echo "Executable schemes and locations:"
-#     echo ""
-#     echo "    - paths checks the following locations for executables in this order:"
-#     echo "    - functions defined interactively"
-#     echo "    - functions defined via standard input into `source`"
-#     echo "    - \$fish_function_path"
-#     echo "    - \$fish_user_paths"
-#     echo "    - \$PATH"
-#     echo "    - builtins"
-#     echo ""
-#     echo "    Note that some builtins will appear twice because they are also in an executable path"
-# end
+function ___paths_plugin_wrap_bold
+    set_color normal
+    echo -n (set_color --bold)"$argv[1..]"
+    set_color normal
+end
+
+function ___paths_plugin_help
+    echo (___paths_plugin_wrap_bold "NAME")
+    echo "paths - executable matches in shell paths or fish autoload."
+    echo ""
+    echo (___paths_plugin_wrap_bold "USAGE")
+    echo ""
+    echo "\
+        paths [-c|-s|-k|-f|-v] <name>"
+    echo ""
+    echo (___paths_plugin_wrap_bold "ARGUMENTS")
+    echo ""
+    echo "\
+        <name> - name of a fish autoload function, function, shell script, executable or builtin"
+    echo ""
+    echo (___paths_plugin_wrap_bold "OPTIONS")
+    echo ""
+    echo "    -k, --no-color
+        Output without color"
+    echo ""
+    echo "    -c, --clean
+        Output without tick marks, headers or saymlink destination.
+        Implies -k."
+    echo ""
+    echo "    -s, --single
+        Output the first result. Implies -k -c"
+    echo ""
+    echo "    -l, --list
+        Show all the locations fish looks for executables"
+    echo ""
+    echo "    -e, --fail-if-not-path
+        If the first result that would be output is not an executable file
+        then return nothing and exit with status 1. Use if you want to make sure
+        that the output is executable before you run it. Implies -s."
+    echo ""
+    echo "    -v, --version
+        Display version number"
+    echo ""
+    echo "    -h, --help
+        Output this help text"
+    echo ""
+    echo (___paths_plugin_wrap_bold "EXPLAINATION")
+    echo ""
+    echo "\
+        paths is a fish function that takes a command name and walks
+        through each of the executable locations to see where the command will
+        execute from. Once found, it continues to find each subsequent location
+        that are next in line were the first command be removed (with exceptions*).
+        Commands are listed in priority order with a heading for each group of
+        executable listing."
+    echo ""
+    echo (___paths_plugin_wrap_bold "LIMITATIONS")
+    echo ""
+    echo "\
+        For declared functions such as those loaded by config.fish only the
+        latest sourced function will be listed. i.e. if the function is overwritten,
+        paths will not be able to tell what it was before it was overwritten. 
+        In the event of a function defined via stdin into the source command,
+        the output will path will be '-' unless the `--fail-if-not-path` option
+        is set."
+    echo ""
+    echo (___paths_plugin_wrap_bold "EXECUTABLE SCHEMES AND LOCATIONS")
+    echo ""
+    echo "\
+        paths checks the following locations for executables in this order:
+
+        1) functions defined interactively
+        2) functions defined via standard input into `source`
+        3) \$fish_function_path
+        4) \$fish_user_paths
+        5) \$PATH
+        6) builtins"
+    echo ""
+    echo "Note that some builtins will appear twice because they are also in an executable path"
+end
+
+function ___paths_plugin_short_help
+    echo "executable matches in shell paths or fish autoload."
+
+    echo (___paths_plugin_wrap_bold "USAGE") "paths [-c|-s|-k|-f|-v] [NAME]"
+    echo ""
+    echo (___paths_plugin_wrap_bold "ARGUMENTS")
+    echo "       [NAME] - name of a fish autoload function, function, shell script, executable or builtin"
+    echo ""
+    echo (___paths_plugin_wrap_bold "OPTIONS")
+    echo "    -h, --help                   Show full help"
+    echo "    -k, --no-color               Output without color"
+    echo "    -c, --clean                  Clean output. Implies -k."
+    echo "    -s, --single                 Output the first result. Implies -k -c"
+    echo "    -l, --list                   Show all places fish looks for executables"
+    echo "    -e, --fail-if-not-path       Return false if output not executable file. Implies -s"
+    echo "    -v, --version.               Display version number"
+    echo ""
+end
 
 function ___paths_plugin_wrap_color
     set_color normal
@@ -155,35 +201,47 @@ function paths --description "Reveal the executable matches in shell paths or fi
     set -a options (fish_opt -s v -l version)
     set -a options (fish_opt -s n -l inline)
     set -a options (fish_opt -s l -l list)
+    set -a options (fish_opt -s h -l help)
+    set -a options (fish_opt -s e -l "fail-if-not-path")
     argparse $options -- $argv
+
+    if set -q _flag_h
+        ___paths_plugin_help
+        and return 0
+    end
 
     if set -q _flag_l
         set input " "
     else
         if test (count $argv) -lt 1
-            set -f SCRIPTNAME (status function)
-            echo "paths - executable matches in shell paths or fish autoload."
-            and echo "usage: paths [-c|-s|-k] <name>"
-            and echo -e "\t-k or --no-color: output without color"
-            and echo -e "\t-c or --clean: output without tick marks or headers. Just a list of paths"
-            and echo -e "\t-s or --single: output path in a single clean line. Implies -k and -c"
-            and echo -e "\t-l or --list: show all the locations fish looks for executables"
-            and echo -e "\t-v or --version: display version number"
-            and echo -e ""
-            # and echo -e "\t-n or --inline: output without endline"`
+            ___paths_plugin_short_help
             and return 1
         end
     end
+
+    if test (count $argv) -lt 1
+        ___paths_plugin_help
+        and return 1
+    end
+
     set -f foundStatus 1
     set -f input (string trim -- $argv)
 
     if set -q _flag_v
-        echo "paths v$VERSION"
+        echo "paths plugin version $VERSION"
+        and return 0
     end
+
+    set -f foundStatus 1
+    set -f input (string trim -- $argv)
 
     # deprecated
     if set -q _flag_q
         set _flag_c True
+    end
+
+    if set -q _flag_e
+        set _flag_s True
     end
 
     if set -q NO_COLOR
